3649448384d921d9d2c5def9bdfc87c4c3b85839
siskanuriaprilia <aprilias878@gmail.com>
2023-10-12 13:15:09 +0700

    WhileKelipatan27_siska

---
+++ WhileKelipatan27.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class WhileKelipatan27 {
+
+
+    public static void main (String [] args) {
+        Scanner scan = new Scanner (System.in);
+
+        int kelipatan= 0, total= 0, counter= 0;
+        double rata_rata;
+
+        System.out.println("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+
+        int i = 1;
+
+        while (i <= 50){
+            if (i % kelipatan == 0 ) {
+                total += i;
+                counter++;
+        }
+        i++;
+        }
+        double rataRata = (double) total / counter;
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);
+    }
+}
